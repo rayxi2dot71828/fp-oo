5589d76fb81f3731d224d7a4c3a38c2ae014642a
rayxi2-71828 <rayxi2-71828@users.noreply.github.com>
2014-09-18 10:51:34 +0800

    Update just-enough-clojure.clj

--- solutions/just-enough-clojure.clj
+++ solutions/just-enough-clojure.clj
@@ -1,6 +1,8 @@
+;;; Exercise 1
 ;; returns the second element of a list
 (def second (fn [list] (nth list 1)))
 
+;;; Exercise 2
 ;; returns the third element of a list
 ; first implementation
 (def third (fn [list] (nth list 2)))
@@ -8,9 +10,18 @@
 ; second implementation
 (def third (fn [list] (first (rest (rest list)))))
 
+;;; Exercise 3
 ; what the name says
 (def add-squares (fn [& args] (apply + (map * args args))))
 
+;;; Exercise 4
 ; factorial without iteration or recursion
 (def bizarre-factorial (fn [n] (apply * (range 1 (inc n)))))
 
+;;; Exercise 5
+;; my own version of take (http://clojuredocs.org/clojure_core/clojure.core/take)
+(def my-take
+  (fn [n coll]
+    (if (or (empty? coll) (<= n 0))
+      ()
+      (cons (first coll) (my-take (dec n) (rest coll))))))
